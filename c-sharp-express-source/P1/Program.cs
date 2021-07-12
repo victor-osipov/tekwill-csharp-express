1d19f550802dd6067ed3ea94eda0fac9df0040f6
Victor Osipov <victor.osipov@outlook.com>
2021-07-12 15:09:24 +0300

    P1 source
    -added P1 source (initial)

---
+++ c-sharp-express-source/P1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to C# Express Course solution and P1 application");
+
+            Console.WriteLine("This is a very simple C# console app");
+
+            Console.WriteLine("Let's display current system date time");
+
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
