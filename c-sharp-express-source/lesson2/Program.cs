e502454f9b14d621cda6cb7f156499aaeda23893
Victor Osipov <vosipov@inthergroup.com>
2021-08-19 20:12:28 +0300

    added lesson2 example
    -uncomment commented block for second version

---
+++ c-sharp-express-source/lesson2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number");
+
+            var line = Console.ReadLine();
+
+            Console.WriteLine("You entered");
+            Console.WriteLine(line);
+
+            var line_double = double.Parse(line);
+
+            var koren = Math.Sqrt(line_double);
+
+            Console.WriteLine("SQRT:");
+            Console.WriteLine(koren);
+
+        }
+
+        //static void Main()
+        //{
+        //    Console.WriteLine("Hello World 1!");
+
+        //    int a, b, c;
+
+        //    a = 7;
+
+        //    b = a;
+
+        //    c = b++;
+
+        //    b = a + b * c;
+        //    //c = a >= 100 ? b : c / 10;
+        //    a = (int)Math.Sqrt(9);
+
+        //    string s = "String stroka";
+
+        //    //Student s1;
+
+        //    char l = 'b';
+
+        //    //var numbers = new List<int>(new[] { 1, 2, 3 });
+        //    //b = numbers.FindLast(n => n > 1);
+        //}
+    }
+
+    class Student { }
+
+    class Mark { }
+
+}
